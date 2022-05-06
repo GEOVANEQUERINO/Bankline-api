a7ef64fd278a0d788792227d216fa10deee0879b
Geovane Querino Valentim <50406715+GEOVANEQUERINO@users.noreply.github.com>
2022-05-06 00:07:22 -0300

    VERSAO FINAL PROJETO

--- estudos/src/main/java/com/dio/santander/banklineapi/controllers/MovimentacaoController.java
+++ estudos/src/main/java/com/dio/santander/banklineapi/controllers/MovimentacaoController.java
@@ -1,32 +1,36 @@
 package com.dio.santander.banklineapi.controllers;
 
+import com.dio.santander.banklineapi.dto.NovaMovimentacao;
 import com.dio.santander.banklineapi.dto.NovoCorrentista;
 import com.dio.santander.banklineapi.model.Correntista;
+import com.dio.santander.banklineapi.model.Movimentacao;
 import com.dio.santander.banklineapi.repository.CorrentistaRepository;
+import com.dio.santander.banklineapi.repository.MovimentacaoRepository;
 import com.dio.santander.banklineapi.service.CorrentistaService;
+import com.dio.santander.banklineapi.service.MovimentacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/correntistas")
+@RequestMapping("/movimentacoes")
 
-public class CorrentistaController {
+public class MovimentacaoController {
     @Autowired
-    private CorrentistaRepository repository;
+    private MovimentacaoRepository repository;
 
     @Autowired
-    private CorrentistaService service;
+    private MovimentacaoService service;
 
     @GetMapping
-    public List<Correntista> findAll () {
+    public List<Movimentacao> findAll () {
         return repository.findAll();
     }
 
     @PostMapping
-    public void save(@RequestBody NovoCorrentista correntista){
-        service.save(correntista);
+    public void save(@RequestBody NovaMovimentacao movimentacao){
+        service.save(movimentacao);
     }
 
 }
